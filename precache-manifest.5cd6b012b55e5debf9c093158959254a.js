07e144c3e720b30dd3e8f476f07066c7437bb504
Sithi Nasila <Sithi.nasila@gmail.com>
2020-07-14 21:08:58 +0530

    Updates

---
+++ precache-manifest.5cd6b012b55e5debf9c093158959254a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf29c8c1755eb7d96929390ee1d2b9f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90ca50472c71ecc889de",
+    "url": "/static/css/main.245563cb.chunk.css"
+  },
+  {
+    "revision": "2cd417facf9b059d670e",
+    "url": "/static/js/2.ba1bbb1b.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/static/js/2.ba1bbb1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90ca50472c71ecc889de",
+    "url": "/static/js/main.f8c1b20f.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
